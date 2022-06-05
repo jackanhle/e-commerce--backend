e2469e8217d6e19f5da31b0bd27f78a0826241d2
Jackanhle <lejack164@gmail.com>
2022-06-05 20:11:47 +1000

    e-commerce

---
+++ Develop/models/index.js
@@ -0,0 +1,27 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
